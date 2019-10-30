1e0fd9987ab7ef143da7e1e73a07890a92e266ef
Reena Bano <reenaishtiaq005@gmail.com>
2019-10-31 00:47:06 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
   return (
     <div className="App">
 
-        <Persons />
+
+      <Persons />
 
     </div>
   );
--- src/components/Persons.jsx
+++ src/components/Persons.jsx
@@ -1,41 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Persons.css';
 import Person from './Person';
 
-export default function Persons() {
-    let personData = [
-        {
-            "name": "Busrha",
-            "age": "12",
-            "description": "chef at Mariotte",
-            "isEditMode": true
-        },
-        {
-            "name": "Dua",
-            "age": "2",
-            "description": "Happiness Officer",
-            "isEditMode": false
-        },
-        {
-            "name": "alizay",
-            "age": "29",
-            "description": "Comedian at youtube",
-            "isEditMode": true
-        },
-    ];
-    let enableEdit = () => {
-        alert("Hi.I am your grandFather")
+export default class Persons extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            personData: [
+                {
+                    "name": "Busrha",
+                    "age": "12",
+                    "description": "chef at Mariotte",
+                    "isEditMode": false
+                },
+                {
+                    "name": "Dua",
+                    "age": "2",
+                    "description": "Happiness Officer",
+                    "isEditMode": false
+                },
+                {
+                    "name": "alizay",
+                    "age": "29",
+                    "description": "Comedian at youtube",
+                    "isEditMode": false
+                }
+            ]
+        }
     }
 
-    return (
-        <div className="Persons">
+    enableEdit = (personClicked) => {
+        this.setState({
+            personsData: this.state.personsData.map
+            (pInner => {
+                if(pInner.name === personClicked.name)
+                {
+                    pInner.isEditMode = true;
+                } else {}
+                return pInner;
+            }) 
+        })
+    }
 
-            <h1>Persons</h1>
-            {
-                personData.map(person => <Person 
-                    person={person}
-                    callMeWhenYouWantToEnableEdit = {enableEdit} />)
-            }
-        </div>
-    )
+    // p.isEditMode = true;
+    // console.log(personData);
+    // alert("Hi, from " + p.name);
+
+    render() {
+        return (
+            <div className="Persons">
+
+                <h1>Persons</h1>
+                {
+                    this.state.personData.map(
+                        person => {
+                            return <Person person={person}
+                                callMeWhenYouWantToEnableEdit={
+                                    () => this.enableEdit(person)
+                                } />
+                        }
+                    )
+
+                    // personData.map(person => <Person
+                    // person={person}
+                    // callMeWhenYouWantToEnableEdit={enableEdit} />)
+                }
+            </div>
+        )
+    }
 }
